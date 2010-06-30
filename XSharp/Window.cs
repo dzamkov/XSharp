23c9a22ae03f423f9187060e78e540f4f7efebe9
DmitryZamkov <dmitry94@gmail.com>
2010-06-30 14:51:27 -0400

    Got a window showing, and also, found that I can successfully push to github

---
+++ XSharp/Window.cs
@@ -0,0 +1,46 @@
+﻿//----------------------------------------
+// Copyright (c) 2010, Dmitry Zamkov
+// Open source under the new BSD License
+//----------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Input;
+using OpenTK.Graphics.OpenGL;
+
+namespace DHTW
+{
+    /// <summary>
+    /// Main window for the applications
+    /// </summary>
+    public class Window : GameWindow
+    {
+        public Window() : base(640, 480, GraphicsMode.Default, "XSharp")
+        {
+
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+            this.SwapBuffers();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Main entry point of the application, if you didnt already get that.
+        /// </summary>
+        public static void Main(string[] Args)
+        {
+            Window win = new Window();
+            win.Run(60.0);
+        }
+    }
+}
